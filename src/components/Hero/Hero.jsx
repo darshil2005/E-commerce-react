3e1b435c41183d4d77815fc4246d06ad073a5932
darshil2005 <darshilbhalala12@gmail.com>
2024-02-13 11:00:16 +0530

    Done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import hand_icon from '../Assets/hand_icon.png';
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="left">
+        <h2>NEEW ARRIVALS ONLY</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
