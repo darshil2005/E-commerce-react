372c65332ead7f1e2e0f11a383cdfbe28fcbb976
darshil2005 <darshilbhalala12@gmail.com>
2024-02-15 16:20:22 +0530

    Shop and other 3 Pages Done

--- src/Pages/shopCategory.jsx
+++ src/Pages/shopCategory.jsx
@@ -13,7 +13,7 @@ const ShopCategory = (props) => {
             <span>Showing 1-12</span> out of 36 Products 
           </p>
           <div>
-            Sort by <img src={dropdown_icon} alt="" />
+            Sort by <img src={dropdown_icon} alt="drop_down_icon" />
           </div>
         </div>
         <div>
